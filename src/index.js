621c53a8af1ed785032615e48a8cc88a8c263c5a
Ludovic Lacouture <lacoludo@outlook.com>
2020-09-30 02:03:25 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import '../style/button.scss';
+
+export default class CustomButton extends Component {
+
+    render() {
+        return(
+            <button>Custom button</button>
+        );
+    }
+
+}
